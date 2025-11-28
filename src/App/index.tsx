0c4d95d80c6116a41979e99f6ff4cd80a73186ff
Tung Teng <thetungctn@gmail.com>
2025-11-28 16:00:23 +0700

    About image animation

--- src/App/index.tsx
+++ src/App/index.tsx
@@ -1,10 +1,11 @@
 import Hero from './Hero'
+import About from './About'
 
 const App = () => {
   return (
     <main className="relative min-h-screen w-screen overflow-x-hidden">
       <Hero />
-      <section className="z-0 min-h-screen bg-blue-500" />
+      <About />
     </main>
   )
 }

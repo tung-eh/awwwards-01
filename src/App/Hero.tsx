c4ba3f235944d121d2809df9a80e099722990376
Tung Teng <thetungctn@gmail.com>
2025-11-28 13:19:24 +0700

    Hero scroll trigger

--- src/App/Hero.tsx
+++ src/App/Hero.tsx
@@ -1,9 +1,12 @@
 import { useState, useRef } from 'react'
 import { TiLocationArrow } from 'react-icons/ti'
-import { useGSAP } from '@gsap/react'
 import gsap from 'gsap'
+import { useGSAP } from '@gsap/react'
+import { ScrollTrigger } from 'gsap/all'
 
 import { Button } from 'src/atoms'
+
+gsap.registerPlugin(ScrollTrigger)
 
 const Hero = () => {
   const transformVideoRef = useRef<HTMLVideoElement>(null)
@@ -50,9 +53,30 @@ const Hero = () => {
     }
   )
 
+  useGSAP(() => {
+    gsap.set('#video-frame', {
+      clipPath: 'polygon(14% 0, 72% 0, 88% 90%, 0 95%)',
+      borderRadius: '0% 0% 40% 10%',
+    })
+    gsap.from('#video-frame', {
+      clipPath: 'polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)',
+      borderRadius: '0% 0% 0% 0%',
+      ease: 'power1.inOut',
+      scrollTrigger: {
+        trigger: '#video-frame',
+        start: 'center center',
+        end: 'bottom center',
+        scrub: true,
+      },
+    })
+  })
+
   return (
     <div className="relative h-screen w-screen overflow-x-hidden">
-      <div className="relative z-10 h-screen w-screen overflow-hidden rounded-lg bg-blue-75">
+      <div
+        id="video-frame"
+        className="relative z-10 h-screen w-screen overflow-hidden rounded-lg bg-blue-75"
+      >
         <div>
           <div className="mask-clip-path absolute-center z-50 size-64 cursor-pointer overflow-hidden rounded-lg">
             <div

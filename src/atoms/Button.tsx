e16629e7c7bb60a722796ed409fd9698f8090589
Tung Teng <thetungctn@gmail.com>
2025-11-28 16:31:51 +0700

    Add tailwind-merge dependencies

--- src/atoms/Button.tsx
+++ src/atoms/Button.tsx
@@ -1,5 +1,5 @@
 import { type ReactElement } from 'react'
-import clsx from 'clsx'
+import { twMerge } from 'tailwind-merge'
 
 const Button = ({
   id,
@@ -17,7 +17,7 @@ const Button = ({
   return (
     <button
       id={id}
-      className={clsx(
+      className={twMerge(
         'group relative z-10 w-fit cursor-pointer overflow-hidden rounded-full bg-violet-50 px-7 py-3 text-black',
         className
       )}

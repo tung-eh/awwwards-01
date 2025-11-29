e848a581f95c25da1deb2b4ef1a6f6010fbfa33b
Tung Teng <thetungctn@gmail.com>
2025-11-29 12:26:31 +0700

    Basic Navbar

---
+++ src/App/Navbar.tsx
@@ -0,0 +1,42 @@
+import { TiLocationArrow } from 'react-icons/ti'
+
+import { Button } from 'src/atoms'
+
+const navItems = ['Nexus', 'Vault', 'Prologue', 'About', 'Contact']
+
+const Navbar = () => {
+  return (
+    <div className="fixed inset-x-0 top-4 z-50 h-16 border-none transition-all duration-700 sm:inset-x-6">
+      <header className="absolute top-1/2 w-full -translate-y-1/2">
+        <nav className="flex size-full items-center justify-between p-4">
+          <div className="flex items-center gap-7">
+            <img src="/img/logo.png" alt="logo" className="w-10" />
+
+            <Button
+              id="product-button"
+              title="Products"
+              rightIcon={<TiLocationArrow />}
+              className="bg-blue-50 md:flex hidden items-center justify-center gap-1"
+            />
+          </div>
+
+          <div className="flex h-full items-center">
+            <div className="hidden md:block">
+              {navItems.map((item, index) => (
+                <a
+                  key={index}
+                  href={`#${item.toLowerCase()}`}
+                  className="nav-hover-btn"
+                >
+                  {item}
+                </a>
+              ))}
+            </div>
+          </div>
+        </nav>
+      </header>
+    </div>
+  )
+}
+
+export default Navbar
--- src/App/index.tsx
+++ src/App/index.tsx
@@ -1,9 +1,11 @@
+import Navbar from './Navbar'
 import Hero from './Hero'
 import About from './About'
 
 const App = () => {
   return (
     <main className="relative min-h-screen w-screen overflow-x-hidden">
+      <Navbar />
       <Hero />
       <About />
     </main>

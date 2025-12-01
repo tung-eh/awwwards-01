3c429fa54f39e0dd27fcaa76e39d03ca0b670af6
Tung Teng <thetungctn@gmail.com>
2025-12-01 17:31:41 +0700

    Transform story img on hover

--- src/App/Story.tsx
+++ src/App/Story.tsx
@@ -1,9 +1,45 @@
+import type { MouseEvent } from 'react'
 import { useRef } from 'react'
+import gsap from 'gsap'
 
 import { AnimatedTitle } from 'src/atoms'
 
 const Story = () => {
   const frameRef = useRef<HTMLImageElement>(null)
+
+  const handleMouseMove = (e: MouseEvent<HTMLDivElement>) => {
+    const { clientX, clientY } = e
+    const element = frameRef.current
+
+    if (!element) return
+
+    const rect = element.getBoundingClientRect()
+    const xPos = clientX - rect.left
+    const yPos = clientY - rect.top
+
+    const centerX = rect.width / 2
+    const centerY = rect.height / 2
+
+    const rotateX = ((yPos - centerY) / centerY) * -10
+    const rotateY = ((xPos - centerX) / centerX) * 10
+
+    gsap.to(element, {
+      duration: 0.3,
+      rotateX,
+      rotateY,
+      transformPerspective: 500,
+      ease: 'power1.inOut',
+    })
+  }
+
+  const handleMouseLeave = () => {
+    gsap.to(frameRef.current, {
+      duration: 0.3,
+      rotateX: 0,
+      rotateY: 0,
+      ease: 'power1.inOut',
+    })
+  }
 
   return (
     <div id="story" className="min-h-dvh w-screen bg-black text-blue-50">
@@ -23,6 +59,8 @@ const Story = () => {
               <div className="story-img-content">
                 <img
                   ref={frameRef}
+                  onMouseMove={handleMouseMove}
+                  onMouseLeave={handleMouseLeave}
                   src="/img/entrance.webp"
                   alt="entrance.webp"
                   className="object-contain"

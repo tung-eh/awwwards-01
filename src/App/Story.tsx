6ead0d49ddbed4ab3ca817b7b751b582b57eb2de
Tung Teng <thetungctn@gmail.com>
2025-12-01 17:40:52 +0700

    Story section description

--- src/App/Story.tsx
+++ src/App/Story.tsx
@@ -2,7 +2,7 @@ import type { MouseEvent } from 'react'
 import { useRef } from 'react'
 import gsap from 'gsap'
 
-import { AnimatedTitle } from 'src/atoms'
+import { AnimatedTitle, Button } from 'src/atoms'
 
 const RoundedCorners = () => {
   return (
@@ -90,6 +90,18 @@ const Story = () => {
               </div>
             </div>
             <RoundedCorners />
+          </div>
+        </div>
+
+        <div className="-mt-80 flex w-full justify-center md:-mt-64 md:me-44 md:justify-end">
+          <div className="flex h-full w-fit flex-col items-center md:items-start">
+            <p className="mt-3 max-w-sm text-center font-circular-web text-violet-50 md:text-start">
+              Where realms converge, lies Zentry and the boundless pillar.
+              Discover its secrets and shape your fate amidst infinite
+              opportunities.
+            </p>
+
+            <Button id="realm-btn" title="discover prologue" className="mt-5" />
           </div>
         </div>
       </div>

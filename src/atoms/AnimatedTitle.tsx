82395e58a443548de850150f1cd85acde93ba0d6
Tung Teng <thetungctn@gmail.com>
2025-12-02 12:05:04 +0700

    Fix: Only first AnimatedTitle working
    Add `scope` to useGSAP hook

--- src/atoms/AnimatedTitle.tsx
+++ src/atoms/AnimatedTitle.tsx
@@ -15,27 +15,33 @@ const AnimatedTitle = ({
 }) => {
   const containerRef = useRef(null)
 
-  useGSAP(() => {
-    const titleAnimation = gsap.timeline({
-      scrollTrigger: {
-        trigger: containerRef.current,
-        start: '100 bottom',
-        end: 'center bottom',
-        toggleActions: 'play none none reverse',
-      },
-    })
+  useGSAP(
+    () => {
+      const titleAnimation = gsap.timeline({
+        scrollTrigger: {
+          trigger: containerRef.current,
+          start: '100 bottom',
+          end: 'center bottom',
+          toggleActions: 'play none none reverse',
+        },
+      })
 
-    titleAnimation.to(
-      '.animated-word',
-      {
-        opacity: 1,
-        transform: 'translate3d(0, 0, 0) rotateY(0deg) rotateX(0deg)',
-        ease: 'power2.inOut',
-        stagger: 0.02,
-      },
-      0
-    )
-  })
+      titleAnimation.to(
+        '.animated-word',
+        {
+          opacity: 1,
+          transform: 'translate3d(0, 0, 0) rotateY(0deg) rotateX(0deg)',
+          ease: 'power2.inOut',
+          stagger: 0.02,
+        },
+        0
+      )
+    },
+    {
+      scope: containerRef,
+      dependencies: [],
+    }
+  )
 
   return (
     <div

b8ff31073b572e08fcace898491bc63a97959bd3
Tung Teng <thetungctn@gmail.com>
2025-12-02 13:50:54 +0700

    Update footer bg color

--- src/App/Footer.tsx
+++ src/App/Footer.tsx
@@ -9,7 +9,7 @@ const socialLinks = [
 
 const Footer = () => {
   return (
-    <footer className="w-screen bg-linear-to-b from-10% to-100% from-violet-300 to-violet-400 py-4 text-black">
+    <footer className="w-screen bg-violet-500 py-4 text-black">
       <div className="container mx-auto flex flex-col items-center justify-between gap-4 px-4 md:flex-row">
         <p className="text-center text-[10px] font-light uppercase md:text-left">
           ©Tung Teng 2025. All rights reserved
